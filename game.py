ac7ffbd24e127ac30a84a1b5c86580c58c298f29
Angela Norris <cocoanorris@yahoo.com>
2019-09-18 15:59:00 -0400

    intro use of while loop

--- game.py
+++ game.py
@@ -20,23 +20,29 @@ monster ={
   'attack': 12,
   'health': 100
 }
+#booleans easily help control while loops
+game_running = True
 
-# use print statement to create user interface using input method
-print('Please select action: ')
-print('1) Attack')
-print('2) Heal')
-player_choice = input()
 
-if player_choice == '1':
-    monster['health'] = monster['health'] - player['attack']
-    player['health'] = player['health'] - monster['attack']
-    pattack_result = "New monster health is {}"
-    mattack_result = "New player health is {}"
-    print(pattack_result.format(monster['health']))
-    print(mattack_result.format(player['health']))
+while game_running == True:
+  # use print statement to create user interface using input method
+  print('Please select action: ')
+  print('1) Attack')
+  print('2) Heal')
+  player_choice = input()
 
-elif player_choice == '2':
-  print('Heal player')
-else:
-  print('Invalid selection')
+  if player_choice == '1':
+      monster['health'] = monster['health'] - player['attack']
+      player['health'] = player['health'] - monster['attack']
+      pattack_result = "New monster health is {}"
+      mattack_result = "New player health is {}"
+      print(pattack_result.format(monster['health']))
+      print(mattack_result.format(player['health']))
 
+  elif player_choice == '2':
+    print('Heal player')
+  else:
+    print('Invalid selection')
+
+  if player['health'] <= 0:
+    game_running = False
