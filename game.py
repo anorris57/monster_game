2738d029357e41fadfc0e5b2a02155cce5ab6fa6
Angela Norris <cocoanorris@yahoo.com>
2019-09-18 15:16:27 -0400

    intro dictionaries

--- game.py
+++ game.py
@@ -5,6 +5,15 @@ health = 100 """
 
 #create variable to contain all information
 #use a list
-player = ['Diamond', 10, 16, 100]
+""" player = ['Diamond', 10, 16, 100]
+player2 = ['Ruby', 10, 16, 100] """
 
-print(player)
+#create dictionary use key value pairs
+player ={
+  'name': 'Diamond',
+  'attack': 10,
+  'heal': 16,
+  'health': 100
+}
+
+print(player['attack'])
