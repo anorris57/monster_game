c8b8e0822f6cd34391de5299a9727f1003f51a50
Angela Norris <cocoanorris@yahoo.com>
2019-09-21 16:29:29 -0400

    setting logic to run rounds and end game

--- game.py
+++ game.py
@@ -8,51 +8,61 @@ health = 100 """
 """ player = ['Diamond', 10, 16, 100]
 player2 = ['Ruby', 10, 16, 100] """
 
-#create dictionary use key value pairs
-player ={
-  'name': 'Diamond',
-  'attack': 13,
-  'heal': 16,
-  'health': 100
-}
-monster ={
-  'name': 'Iron',
-  'attack': 12,
-  'health': 100
-}
+
 #booleans easily help control while loops
 game_running = True
 
 
 while game_running == True:
+  new_round = True
+  #create dictionary use key value pairs
+  player ={
+  'name': 'Diamond',
+  'attack': 13,
+  'heal': 16,
+  'health': 100
+  }
+  monster ={
+  'name': 'Iron',
+  'attack': 12,
+  'health': 100
+  }
 
-  player_won = False
-  monster_won = False
+  while new_round == True:
 
-  # use print statement to create user interface using input method
-  print('Please select action: ')
-  print('1) Attack')
-  print('2) Heal')
-  player_choice = input()
+    player_won = False
+    monster_won = False
 
-  if player_choice == '1':
-      monster['health'] = monster['health'] - player['attack']
-      if monster['health'] <= 0:
-        player_won = True
-      else:
-        player['health'] = player['health'] - monster['attack']
-        if player['health'] <= 0:
-          monster_won = True
+    # use print statement to create user interface using input method
+    print('Please select action: ')
+    print('1) Attack')
+    print('2) Heal')
+    print('3) Exit Game')
+    player_choice = input()
 
-      pattack_result = "New monster health is {}"
-      mattack_result = "New player health is {}"
-      print(pattack_result.format(monster['health']))
-      print(mattack_result.format(player['health']))
+    if player_choice == '1':
+        monster['health'] = monster['health'] - player['attack']
+        if monster['health'] <= 0:
+          player_won = True
+        else:
+          player['health'] = player['health'] - monster['attack']
+          if player['health'] <= 0:
+            monster_won = True
 
-  elif player_choice == '2':
-    print('Heal player')
-  else:
-    print('Invalid selection')
+        pattack_result = "New monster health is {}"
+        mattack_result = "New player health is {}"
+        print(pattack_result.format(monster['health']))
+        print(mattack_result.format(player['health']))
 
-  if player_won == True or monster_won == True:
-    game_running = False
+    elif player_choice == '2':
+      print('Heal player')
+
+    elif player_choice == '3':
+      new_round = False
+      game_running = False
+
+    else:
+      print('Invalid selection')
+
+    if player_won == True or monster_won == True:
+      new_round = False
