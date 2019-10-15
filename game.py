ec54d719fd2567932f741339bc118d4beb0584f5
Angela Norris <cocoanorris@yahoo.com>
2019-10-15 16:05:00 -0400

    refactor

--- game.py
+++ game.py
@@ -11,9 +11,10 @@ player2 = ['Ruby', 10, 16, 100] """
 import random
 #booleans easily help control while loops
 game_running = True
+game_results = []
 
-def calculate_monster_attack():
-  return random.randint(monster['attack_min'],monster['attack_max'])
+def calculate_monster_attack(attack_min, attack_max):
+  return random.randint(attack_min,attack_max)
 
 def game_ends(winner_name):
   print(f'{winner_name} won the game!')
@@ -22,6 +23,7 @@ def game_ends(winner_name):
 
 
 while game_running == True:
+  counter = 0
   new_round = True
   #create dictionary use key value pairs
   player ={
@@ -48,6 +50,7 @@ while game_running == True:
 
   while new_round == True:
 
+    counter = counter + 1
     player_won = False
     monster_won = False
 
@@ -57,6 +60,7 @@ while game_running == True:
     print('1) Attack')
     print('2) Heal')
     print('3) Exit Game')
+    print('4) Show Results')
     player_choice = input()
 
     if player_choice == '1':
@@ -65,7 +69,7 @@ while game_running == True:
           player_won = True
         else:
        #   monster_attack = random.randint(10,20)
-          player['health'] = player['health'] - calculate_monster_attack()
+          player['health'] = player['health'] - calculate_monster_attack(monster['attack_min'], monster['attack_max'])
           if player['health'] <= 0:
             monster_won = True
 
@@ -74,7 +78,7 @@ while game_running == True:
     #  player_heal = random.randint(9, 19)
       player['health'] = player['health'] + player['heal']
 
-      player['health'] = player['health'] - calculate_monster_attack()
+      player['health'] = player['health'] - calculate_monster_attack(monster['attack_min'], monster['attack_max'])
       if player['health'] <= 0:
         monster_won = True
 
@@ -82,6 +86,10 @@ while game_running == True:
     elif player_choice == '3':
       new_round = False
       game_running = False
+    
+    elif player_choice =='4':
+      for list_item in game_results:
+        print(list_item)
 
     else:
       print('Invalid selection')
@@ -94,10 +102,14 @@ while game_running == True:
         
     elif player_won:
         game_ends(player['name'])
+        round_result = {'name': player['name'], 'health': player['health'], 'rounds': counter}
+        game_results.append(round_result)
         new_round = False
 
     elif monster_won:
         game_ends(monster['name'])
+        round_result = {'name': player['name'], 'health': player['health'], 'rounds': counter}
+        game_results.append(round_result)
         new_round = False
         
 """     elif player_won:
